938e686906f204f0c6e03091309e12fc979a4381
octaviomds <151770584+octaviomds@users.noreply.github.com>
2025-01-16 10:22:31 +0100

    appli

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
+import {Provider} from 'react-redux';
+import React, {useCallback} from 'react';
+import Loader from './src/components/Loader';
+import {persistor, store} from './src/store';
+import * as SplashScreen from 'expo-splash-screen';
+import FlashMessage from './src/components/FlashMessage';
+import Orientation from 'react-native-orientation-locker';
+import {PersistGate} from 'redux-persist/integration/react';
+import StackNavigator from './src/navigation/StackNavigator';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'DMSans-Bold': require('./src/assets/fonts/DMSans-Bold.ttf'),
+    'DMSans-Medium': require('./src/assets/fonts/DMSans-Medium.ttf'),
+    'DMSans-Regular': require('./src/assets/fonts/DMSans-Regular.ttf'),
+    'Inter-Bold': require('./src/assets/fonts/Inter-Bold.ttf'),
+    'Inter-Medium': require('./src/assets/fonts/Inter-Medium.ttf'),
+    'Inter-SemiBold': require('./src/assets/fonts/Inter-SemiBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+      Orientation.lockToPortrait();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <Provider store={store}>
+        <PersistGate
+          loading={<Loader />}
+          persistor={persistor}
+        >
+          <NavigationContainer>
+            <StackNavigator />
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
+      <FlashMessage />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
